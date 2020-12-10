1b040c055a802c155935d79144c86f90f3e8d8f6
Gatis <gatis.meikulis@gmail.com>
2020-12-10 15:44:41 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.module.scss';
+// import _ from 'lodash';
+
+export const App = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>2048 Game</h1>
+      </div>
+    </div>
+  );
+};
+
